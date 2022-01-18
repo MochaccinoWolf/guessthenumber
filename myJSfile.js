53103df729edb980f2f71cfb59263c1eaf98e358
MochaccinoWolf <MochaccinoWolf@users.noreply.github.com>
2022-01-18 13:32:10 -0500

    initial commit

---
+++ myJSfile.js
@@ -0,0 +1,23 @@
+<script>
+    //learing how to make a java script program
+    //make a simple number count game; https://developer.mozilla.org/en-US/docs/Learn/JavaScript/First_steps/A_first_splash 
+
+    let randomNumber = Math.floor(Math.random() *100 + 1);
+
+    const guesses = document.querySelector('.guesses');
+    const lastResult = document.querySelector('.lastResult');
+    const lowOrHi = document.querySelector('.lowOrHi');
+
+    const guessSubmit = document.querySelector('.guessSubmit');
+    const guessField = document.querySelector('.guessField');
+
+    let guessCount = 1;
+    let resetButton;
+
+    
+
+
+
+
+
+</script>
